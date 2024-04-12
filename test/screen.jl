688125266d388e30a6fc7321f7f3a0a4d5421b91
Tobias Ott <ottt@irs.uni-stuttgart.de>
2024-04-12 15:32:43 +0200

    Improved screen test

--- test/screen.jl
+++ test/screen.jl
@@ -4,8 +4,8 @@ using GLMakie
 using GLMakie.GLFW
 
 FPS = 60
-Npts = 10^6
-Nworkers = 1
+Npts = 10^5
+Nworkers = 3
 
 function busywait(ns)
     time = time_ns()
@@ -26,12 +26,12 @@ struct DataContainer
     end
 end
 
-function waitfor(c::DataContainer; timeout=1000)
+function waitfor(c::DataContainer, term; timeout=1000)
     time = time_ns()
     while !all(c.worker_done)
-        busywait(0)#10^8)
+        busywait(0)
+        term[] && return
         time_ns() - time > timeout*10^6 && return false
-        #println("GUI waiting..., status=", c.worker_done)
     end
     return true
 end
@@ -40,25 +40,19 @@ function requestdata(c::DataContainer)
     c.worker_done .= false
 end
 
-function senddata!(c::DataContainer, data, id)
-    #println("Worker ", id, " sending data! Done=", c.worker_done[id])
+function senddata!(c::DataContainer, data, id, term)
     while c.worker_done[id]
-        busywait(0)#10^8)
+        busywait(0)
+        term[] && return
     end
 
-    write(string("myfile-",id,".txt"),string("offset..."))
 
-    #println("Do")
     offset = c.worker_offsets[id]
-    #println(offset)
     for i in eachindex(data)
         c.data[i + offset] = data[i]
     end
-    #println("Done")
-    write(string("myfile-",id,".txt"),string("done..."))
 
     c.worker_done[id] = true
-    #println("Worker ", id, " sending data done!")
 end
 
 ## Setup screen ---------------------------------------------------------------------------
@@ -70,7 +64,7 @@ scene = Scene(size=window_size)
 
 campixel!(scene)
 
-pts = [Point2(rand.((1:px, 1:py))) for i in 1:Npts]
+pts = [Point2f(100, 200) for i in 1:Npts]
 
 sc = scatter!(scene, pts,
         marker = GLMakie.FastPixel(),
@@ -94,62 +88,66 @@ Ni = Npts ÷ Nworkers
 comm = DataContainer(copy(pts), collect(((1:Nworkers).-1)*Ni))
 println(comm.data[1])
 
-function workerloop(comm, pts, id)
-    time = time_ns()
-    while time_ns() - time < 30*10^9 # Run for 30 sec
-        #println("In workerloop... worker ", id)
+function workerloop(term, comm, pts, id, maxpos)
+    vels = Point2f[]
+    for i in eachindex(pts)
+        r = 0.5 + rand()
+        phi = 2π * rand()
+        vx = 3 + sin(phi) * r
+        vy = 2 + cos(phi) * r
+        push!(vels, Point2f(vx, vy))
+    end
+
+    while !term[]
         for i in eachindex(pts)
-            pts[i] = abs.(pts[i] + Point2f(rand.((-2:2, -2:2))))
+            pts[i] += vels[i]
+            vels[i] = vels[i] .* (0 .< pts[i] .< maxpos) - vels[i] .* (pts[i] .<= 0) - vels[i]               .* (pts[i] .>= maxpos)
+            pts[i]  = pts[i]  .* (0 .< pts[i] .< maxpos) - pts[i]  .* (pts[i] .<= 0) + (2 * maxpos - pts[i]) .* (pts[i] .>= maxpos)
         end
 
-        write(string("myfile-",id,".txt"),string("Sending..."))
+        senddata!(comm, pts, id, term)
 
-        senddata!(comm, pts, id)
         GC.safepoint()
     end
 end
 
-function renderloop(screen, pts, comm)
-    while !terminate[]
+function renderloop(screen, pts, comm, term)
+    while !term[]
+        #println(1)
         time = time_ns()
-
-        success = waitfor(comm; timeout=1000)
-
+        #println(2)
+        success = waitfor(comm, term; timeout=1000)
+        #println(3)
         wait_time = time_ns()
-
+        #println(4)
         if success
-            for i in eachindex(pts[])
-                pts[][i] = comm.data[i]
-            end
-
+            pts[] = comm.data
             requestdata(comm)
         else
             println("WTF")
         end
-
-
-        notify(pts)
-
+        #println(5)
         cpu_time = time_ns()
-
         GLMakie.pollevents(screen)
+        gpu1_time = time_ns()
         GLMakie.render_frame(screen)
+        gpu2_time = time_ns()
         GLFW.SwapBuffers(glscreen)
-
         gpu_time = time_ns()
-
+        #println(6)
         if  time + 10^9/FPS - gpu_time < 0
             println(
                 "FPS=", round(10^9/(time_ns() - time)),
                 " | Workers:", round((wait_time - time)/(gpu_time - time)*100; digits=2),
                 " | CPU:", round((cpu_time - wait_time)/(gpu_time - time)*100; digits=2),
-                " | GPU:", round((gpu_time - cpu_time)/(gpu_time - time)*100; digits=2),
+                " | GPU1:", round((gpu1_time - cpu_time)/(gpu_time - time)*100; digits=2),
+                " | GPU2:", round((gpu2_time - gpu1_time)/(gpu_time - time)*100; digits=2),
+                " | GPU3:", round((gpu_time - gpu2_time)/(gpu_time - time)*100; digits=2),
             )
-            println((cpu_time - time)/10^9)
+            #println((cpu2_time - time)/10^9)
         end
         busywait(time + 10^9/FPS - time_ns())
     end
-
     close(screen)
 end
 
@@ -157,7 +155,10 @@ end
 for i in 1:Nworkers
     start = comm.worker_offsets[i] + 1
     stop = i == Nworkers ? length(pts) : comm.worker_offsets[i+1]
-    Threads.@spawn workerloop(comm, pts[start:stop], i)
+    Threads.@spawn workerloop(terminate, comm, pts[start:stop], i, Point2(window_size))
 end
 
-renderloop(screen, sc[1], comm)
+# 40 - 60
+# This ignores the apply_transform... (Swap back when changing the cam to the physical lengths)
+renderloop(screen, sc[1].listeners[1].second.result, comm, terminate)
+#renderloop(screen, sc[1], comm)
