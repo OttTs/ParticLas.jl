b80d03c59e12ba1748d4bf5da5fafdd8350cdedd
Tobias Ott <ottt@irs.uni-stuttgart.de>
2024-04-26 15:01:07 +0200

    Updated communication between threads

--- src/communication.jl
+++ src/communication.jl
@@ -1,73 +1,95 @@
-# TODO Busywait is actually only used in the renderloop for correct 60 fps
-function busywait(ns)
-    time = time_ns()
-    while time_ns() - time < ns; end
-end
-
 #=
 A Barrier is used to synchronize all worker threads.
 When "synchronize!" is called, the thread adds one to a counter.
 If all threads have added a counter, synchronization is done, otherwise wait.
 =#
 
-struct Barrier{T}
-    _condition::T
-    _counter :: Base.RefValue{Int8}
+mutable struct Barrier
+    _condition::Base.GenericCondition{ReentrantLock}
+    _counter::Int8
     _num_threads::Int8
     function Barrier(num_threads)
         _condition = Threads.Condition()
-        return new{typeof(_condition)}(_condition, Base.RefValue{Int8}(0), num_threads)
+        return new(_condition, Base.RefValue{Int8}(0), num_threads)
     end
 end
 
 function synchronize!(b::Barrier)
-    lock(b._condition)
-    try
-        b._counter[] += 1
-        if b._counter[] >= b._num_threads
+    lock(b._condition) do
+        b._counter += 1
+        if b._counter >= b._num_threads
             notify(b._condition)
-            b._counter[] = 0
+            b._counter = 0
         else
-            wait(b._condition)
+            wait(b._condition, first=true)
         end
-    finally
-        unlock(b._condition)
     end
 end
 
-
-# Channels...
-#
 #=
-From the interactive thread, the workers need:
-- pause / reset (particles, walls) / terminate
-- walls
-- Inflow condition (velocity, density)
-- Wall accomodation_coefficient
-- do collisions
+A data channel is used to communicate between the GUI and the worker thread.
+Each thread gets its own channel to send particle / mesh data,
+If requested is true, the sender can insert data to the cannel, otherwise wait!
+If requested is false, the receiver can read the data, otherwise wait
 
-From the workers, the interactive thread needs
-- Particle positions
-or
-- density / velocity / temperature
-
-What is always the same?
-
+Senders can edit their data as they want until they are done!
+Then the receiver calls receive!() which swaps the data!
 =#
+mutable struct DataChannel{T}
+    _condition::Vector{Base.GenericCondition{ReentrantLock}}
+    _requested::Vector{Bool}
+    _sender_data::T
+    _receiver_data::T
+    DataChannel(T, num_senders=1) = new(
+        [Threads.Condition() for _ in 1:num_senders],
+        zeros(Bool, num_senders),
+        T(), T()
+    )
+end
 
-struct SettingsData
-    terminate::Bool
-    pause::Bool
-    delete_particles::Bool
-    delete_walls::Bool
+data(c::DataChannel) = c._receiver_data
 
-    do_particle_collisions::Bool
+#=
+Example usage:
+send!(channel, id) do data
+    # change data here
+end
+=#
+function send!(f, c::DataChannel, id=1)
+    lock(c._condition[id]) do
+        while !c._requested[id]
+            wait(c._condition[id], first=true)
+        end
+        f(c._sender_data)
+        c._requested[id] = false
+        notify(c._condition[id])
+    end
+end
 
-    drawn_wall::NTuple{2, Point2{Float64}}
-    accomodation_coefficient::Float64
+#=
+Swaps the sender and receiver data.
+After calling receive!, the new data can be accessed by calling data(c::DataChannel)
+=#
+function receive!(c::DataChannel)
+    # Wait for all senders to complete
+    for i in 1:length(c._condition)
+        lock(c._condition[i]) do
+            while c._requested[i]
+                wait(c._condition[i], first=true)
+            end
+        end
+    end
 
-    inflow_density::Float64
-    inflow_velocity::Float64
+    # Swap sender data and receiver data!
+    tmp = c._receiver_data
+    c._receiver_data = c._sender_data
+    c._sender_data = tmp
 
-    draw_type::Symbol # :particles, :density, :velocity, :temperature
+    # Request new data
+    for i in 1:length(c._condition)
+        lock(c._condition[i]) do
+            c._requested[i] = true
+            notify(c._condition[id])
+        end
+    end
 end
--- src/simulation.jl
+++ src/simulation.jl
@@ -3,25 +3,44 @@ include("simulation/statistics.jl")
 
 
 
-function simulation_thread(thread_id)
-    while !terminate
-        if !pause
+function simulation_thread(
+    particles,
+    species,
+    inflow,
+    mesh,
+    time_step,
+    thread_id,
+    barrier,
+    settings,
+    plotting_data
+)
+    while !data(settings).terminate
+        if !data(settings).pause
             # Local particles only!
             insert_particles(particles, inflow, mesh, time_step)
-            movement_step!(particles, time_step, mesh, wall_condition)
-            # TODO send particle data here
+            movement_step!(particles, time_step, mesh, data(settings).wall_condition)
             deposit!(particles, mesh, thread_id)
-            settings.plot_type == :particles && send_particle_data!(particles, settings)
+            send_particle_data!(particles, plotting_data)
             synchronize!(barrier)
 
-            # TODO send mesh data during collision step
-            collision_step!(particles, time_step, species, mesh, thread_id, settings)
+            collision_step!(
+                particles,
+                time_step,
+                species,
+                mesh,
+                thread_id,
+                plotting_data # Send data during collision
+            )
         end
-        reset_walls && delete_walls!(mesh, thread_id)
+        !data(settings).delete_walls && delete_walls!(mesh, thread_id)
         synchronize!(barrier)
-        thread_id == 1 && receive_settings()
+        thread_id == 1 && receive_settings_data!(settings)
+
+
+        # Add wall and set inflow?
         synchronize!(barrier)
-        reset_particles && clear!(particles)
+
+        !data(settings).delete_particles && clear!(particles)
     end
 end
 
@@ -31,6 +50,7 @@ Here, the particles are added to the list of particles in each cell.
 This is done for the collision step, where each cell needs to know its particles.
 =#
 function deposit!(particles, mesh, thread_id)
+    delete_particles!(mesh, thread_id)
     for particle in particles
         index = get_index(particle.position, mesh)
         particles_in_cell = local_list(mesh.cells[index].particles, thread_id)
