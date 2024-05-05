8513bb46f593c46ae562463e2451493318cb00ea
OttTs <t.ott.marbach@web.de>
2024-05-05 12:38:06 +0200

    Added create_app function

--- src/ParticLas.jl
+++ src/ParticLas.jl
@@ -1,6 +1,6 @@
 module ParticLas
 
-export run_particlas
+export create_app, run_particlas
 
 using GLMakie: Point2, Point2f, Vec, Vec2, Vec3, RGBf, Observable
 import GLMakie
@@ -9,6 +9,8 @@ using LinearAlgebra
 using StaticArrays: @SMatrix
 using SpecialFunctions: erf
 using Printf: @sprintf
+
+import PackageCompiler
 
 mutable struct TimingData
     gui_start::Float64
@@ -32,7 +34,32 @@ include("communication.jl")
 include("simulation.jl")
 include("gui.jl")
 
+
+"""
+    create_app(dst=nothing)
+
+Create an executable to run ParticLas as a standalone app.
+Use the optional argument `dst` to specify the path of the compiled app.
+
+# Examples
+```julia-repl
+julia> create_app("/home/myuser/ParticLasApp")
+julia> create_app("C:\\Program Files\\ParticLasApp")
+```
+"""
+function create_app(dst=nothing)
+    pkg_path = string(split(pathof(ParticLas), "src")[1])
+    isnothing(dst) && (dst = string(pkg_path, "ParticLasApp"))
+    PackageCompiler.create_app(pkg_path, dst, include_lazy_artifacts=true)
+    # TODO Precompilation script!
+end
+
 # TODO num_threads is given by Threads.nthreads(:default)
+
+function julia_main()::Cint
+    run_particlas()
+    return 0
+end
 
 function run_particlas()
     gui_data = setup_gui()
